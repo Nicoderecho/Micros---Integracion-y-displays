dfc239e162fecf89d91471ae550ba9b42847658a
Nicoderecho <167894724+Nicoderecho@users.noreply.github.com>
2025-05-05 19:56:20 +0200

    Update Encabezado_displays.h

--- ProgramaPrincipal/Encabezado_displays.h
+++ ProgramaPrincipal/Encabezado_displays.h
@@ -5,3 +5,17 @@
 #define DS_PIN PK7
 
 volatile uint8_t display_activo = 0;  // Variable global que indica qué display está encendido
+
+void init_displays(void) {
+    DDRK = 0xFF;  // PK0-PK7 como salidas
+}
+
+
+// Depende de estado activo un display o el otro
+void activar_display(uint8_t estado) {
+    if (estado == 0) {
+        PORTK &= ~(1 << DS_PIN);  // Ds = 0
+    } else {
+        PORTK |= (1 << DS_PIN);   // Ds = 1
+    }
+}
