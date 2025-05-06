88d1fced815b798dd3ec1bde3dbecef4d89465ad
Nicoderecho <167894724+Nicoderecho@users.noreply.github.com>
2025-05-06 18:23:29 +0200

    Update main.c

--- ProgramaPrincipal/main.c
+++ ProgramaPrincipal/main.c
@@ -1,8 +1,4 @@
-#define F_CPU 8000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-#include "encabezado_displays.h"
-#include "encabezado_sensores.h"
+
 
 #define F_CPU 8000000UL
 #include <avr/io.h>
