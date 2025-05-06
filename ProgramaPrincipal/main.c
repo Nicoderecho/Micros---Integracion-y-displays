82d3f8307edf80c564e8db90177deb8b1502fc6e
Nicoderecho <167894724+Nicoderecho@users.noreply.github.com>
2025-05-06 18:23:11 +0200

    Update main.c

--- ProgramaPrincipal/main.c
+++ ProgramaPrincipal/main.c
@@ -4,14 +4,18 @@
 #include "encabezado_displays.h"
 #include "encabezado_sensores.h"
 
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "encabezado_displays.h"
+#include "encabezado_sensores.h"
+
 int main(void) {
 	init_displays();   // Configura pines de displays
 	init_timer0();     // Activa multiplexado con Timer0
 	sensores_init();   // Configura sensores con interrupciones
 
-	// Mostrar 3 y 7 en los displays al arrancar
-	set_display_1(5);
-	set_display_2(5);
+
 
 	while (1) {
 		
