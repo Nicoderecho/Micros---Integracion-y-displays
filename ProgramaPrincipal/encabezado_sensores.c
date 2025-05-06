1373fb18ee358561568bd91d38f09bece93830a0
Nicoderecho <167894724+Nicoderecho@users.noreply.github.com>
2025-05-06 11:43:09 +0200

    Update encabezado_sensores.c
    Cambio en la configuracion de los pines

--- ProgramaPrincipal/encabezado_sensores.c
+++ ProgramaPrincipal/encabezado_sensores.c
@@ -4,12 +4,13 @@
 
 void sensores_init(void) {
     // Configura el puerto K como entrada y activa resistencias pull-up
-    DDRK = 0x00;    // Todo entrada
-    PORTK = 0xFF;   // Pull-ups activados
+    DDRK &= ~((1<<PK0)|(1<<PK1)|(1<<PK2)|(1<<PK3)|(1<<PK4)|(1<<PK5)|(1<<PK6));    // Todo entrada
+    PORTK |= (1<<PK0)|(1<<PK1)|(1<<PK2)|(1<<PK3)|(1<<PK4)|(1<<PK5)|(1<<PK6);   // Pull-ups activados
 
     // Habilita interrupciones por cambio de pin en PK0–PK7 (PCINT16–23)
     PCICR |= (1 << PCIE2);      // Habilita grupo de interrupciones PCI2
-    PCMSK2 |= 0xFF;             // Habilita todos los PCINT del grupo 2 (PCINT16–23)
+    PCMSK2 |= (1<<PCINT16)|(1<<PCINT17)|(1<<PCINT18)|(1<<PCINT19)|(1<<PCINT20)|(1<<PCINT21)|(1<<PCINT22);   // Habilita todos los PCINT del grupo 2 (PCINT16–22)
+
 
     sei();  // Habilita interrupciones globales
 }
@@ -21,8 +22,8 @@ ISR(PCINT2_vect) {
 
 void manejar_bolo_caido(void) {
     // Enciende un LED en PB0 (por ejemplo)
-    DDRB |= (1 << PB0);
-    PORTB |= (1 << PB0);
+    DDRB |= (1 << PL6);
+    PORTB |= (1 << PL6);
 
     // Mostrar el número 1 en el display 1
     set_display_1(1);
