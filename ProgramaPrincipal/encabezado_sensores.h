88a2b240a598875500cf94ca22ec651bf0669b3c
Nicoderecho <167894724+Nicoderecho@users.noreply.github.com>
2025-05-06 18:27:57 +0200

    Update encabezado_sensores.h

--- ProgramaPrincipal/encabezado_sensores.h
+++ ProgramaPrincipal/encabezado_sensores.h
@@ -3,8 +3,14 @@
 
 #include <avr/io.h>
 
+extern volatile uint8_t estados_bolos[6];
+extern volatile uint8_t bolos_caidos;
+
 // Inicializa los pines del puerto K como entrada con pull-up
 void sensores_init(void);
+
+// Funcion para iniciar cuenta de 1 segundo tras tirar bolos
+void iniciar_timer1(void);
 
 // Función que enciende el LED cuando se detecta un bolo caído
 void manejar_bolo_caido(void);
